7d4548567602c1a9c0dd773a173075b242a12950
Marcos Souza <marcos.souza@beeleads.com.br>
2024-02-12 10:38:19 -0300

    Animando abertura dos selects

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,21 @@ const config: Config = {
           25: '#fcfaff',
         },
       },
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: '0', transform: 'translate(-2px)' },
+          to: { opacity: '1', transform: 'translate(0)' },
+        },
+        // slideUpAndFade: {
+        //   from: { opacity: 1 },
+        //   to: { opacity: 0 },
+        // },
+      },
+      animation: {
+        slideDownAndFade:
+          'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideUpAndFade: 'slideUpAndFade 1s linear',
+      },
     },
   },
   plugins: [],
